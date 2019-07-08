a4ec85b966e79c529c78925d64ae05d30042af35
yangxinpeng <972672845@qq.com>
2019-07-08 16:56:00 +0800

    拆分头部和侧边栏，下载引入vue-router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/index.css'
+import '../node_modules/vue-router/dist/vue-router'
+
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
