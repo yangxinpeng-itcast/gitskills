83e6ff92400f9c7e6c7dfed748962821677489ae
yangxinpeng <972672845@qq.com>
2019-07-08 18:26:17 +0800

    配置

--- src/main.js
+++ src/main.js
@@ -7,15 +7,21 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/index.css'
 import VueRouter from 'vue-router'
 
+//引入英雄列表组件在这里匹配路由
+import HeroList from './views/heroes/HeroList'
+
 //vue-router注册到vue中才能使用
 //在全局script标签中不用写这一步(自动完成)
 Vue.use(VueRouter)
+
 
 //创建路由对象，实例化vue-router
 //vue-router使用步骤第4步
 const router = new VueRouter({
     //匹配路由规则
-    routes: []
+    routes: [
+        { name: 'herolist', path: '/herolist', component: HeroList }
+    ]
 })
 
 new Vue({
